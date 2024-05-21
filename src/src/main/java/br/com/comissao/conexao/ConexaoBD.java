ecec7638e7284020e6671e7d961e8fd0aa48def3
Bruno Pires <brunompirescardoso@gmail.com>
2024-05-21 19:43:47 -0300

    feat(): Implementou a classe ConexaoBD abreConexao(); fechaConexao(); Responsável pela abertura e fechamento de conexão do programa com o banco de dados.

--- src/src/main/java/br/com/comissao/conexao/ConexaoBD.java
+++ src/src/main/java/br/com/comissao/conexao/ConexaoBD.java
@@ -1,4 +1,32 @@
 package src.main.java.br.com.comissao.conexao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoBD {
+    Connection conexao;
+
+    private final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private final String usuario = "postgres";
+    private final String senha = "652314";
+    private final String driverBanco = "org.postgresql.Driver";
+
+    public Connection abreConexao() throws SQLException{
+        try{
+            Class.forName(driverBanco);
+            return conexao = DriverManager.getConnection(url, usuario, senha);
+        }catch (Exception excecao){
+            System.out.println("Concexão não foi aberta!");
+        }
+        return conexao;
+    }
+
+    public void fechaConexao() throws SQLException{
+        try{
+            conexao.close();
+        }catch (Exception excecao){
+            System.out.println("Conexao não foi fechada!");
+        }
+    }
 }

036450bc60b0a3c97d0647430db959fa19537ce4
Bruno Pires <brunompirescardoso@gmail.com>
2024-05-23 20:31:44 -0300

    feat(): Implementar a classe Mecânico Adiciona classe responsável pelas caracteristicas dos mecanidos Herda de IFuncionario'

--- src/src/main/java/br/com/comissao/dao/Mecanico.java
+++ src/src/main/java/br/com/comissao/dao/Mecanico.java
@@ -1,4 +1,28 @@
 package src.main.java.br.com.comissao.dao;
 
-public class Mecanico {
+public abstract class Mecanico implements IFuncionario{
+    private int idFuncionario;
+    private String nome;
+    private double saldoComissao;
+
+
+    @Override
+    public int getidFuncionario() {
+        return 0;
+    }
+
+    @Override
+    public String getNome() {
+        return "";
+    }
+
+    @Override
+    public double setSaldoComissao() {
+        return 0;
+    }
+
+    @Override
+    public double getSaldoComissao() {
+        return 0;
+    }
 }

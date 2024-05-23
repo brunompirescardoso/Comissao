dda7143b26441bc4fc9983e344e5811a972bd5b5
Bruno Pires <brunompirescardoso@gmail.com>
2024-05-23 20:28:58 -0300

    feat(): Implementar interface IFuncionario Adiciona a interface responsável pela herança dos tipos de funcionarios

--- src/src/main/java/br/com/comissao/dao/IFuncionario.java
+++ src/src/main/java/br/com/comissao/dao/IFuncionario.java
@@ -1,4 +1,14 @@
 package src.main.java.br.com.comissao.dao;
 
 public interface IFuncionario {
+    public int getidFuncionario();
+
+    public String getNome();
+
+    public double setSaldoComissao();
+
+    public double getSaldoComissao();
+
+
+
 }

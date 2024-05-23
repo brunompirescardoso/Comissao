9dff779839bc21cad4a72950887c9b218261f616
Bruno Pires <brunompirescardoso@gmail.com>
2024-05-23 20:30:49 -0300

    feat(): Implementar a classe Administrador Adiciona classe responsável pelas caracteristicas dos administradores Herda de IFuncionario;

--- src/src/main/java/br/com/comissao/dao/Administrador.java
+++ src/src/main/java/br/com/comissao/dao/Administrador.java
@@ -1,4 +1,36 @@
 package src.main.java.br.com.comissao.dao;
 
-public class Administrador {
+public abstract class Administrador implements IFuncionario{
+    private int id_Funcionario;
+    private String nome;
+    private double saldoComissao;
+    private double salarioFixo;
+
+    public Administrador(double salarioFixo){
+        this.salarioFixo = salarioFixo;
+    }
+
+    @Override
+    public int getidFuncionario() {
+        return 0;
+    }
+
+    @Override
+    public String getNome() {
+        return "";
+    }
+
+    @Override
+    public double setSaldoComissao() {
+        return 0;
+    }
+
+    @Override
+    public double getSaldoComissao() {
+        return 0;
+    }
+
+    public double getSalarioFixo(){
+        return this.salarioFixo;
+    }
 }
